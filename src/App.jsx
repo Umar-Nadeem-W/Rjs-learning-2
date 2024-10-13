7fe8118290994a27ceefb1091f7c928c20d173db
Umar-Nadeem-W <umarnadeem.w@gmail.com>
2024-10-13 17:58:54 +0500

    A very simple banking functionality to practice hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,63 @@
-import { useReducer, useState } from 'react'
+import { useEffect, useReducer, useState , useRef} from 'react'
 
 
 function App() {
-  const [redstate, dispatch] = useReducer(reducer,{count: 0, incrementvalue:  1}) //first argument is a function and second argument is default state value in te useReducer hook
-  function reducer(redstate, action){ // in the reducer function, first argument is the sate, second the action object
-      if(action.type=="increment"){return {...redstate, count: redstate.count + redstate.incrementvalue } }
-      else if(action.type == "decrement"){return {...redstate, count: redstate.count - redstate.incrementvalue }}
-      else if(action.type == "setIncrement"){return {...redstate, incrementvalue: action.payload }}
+    
+    const [danger, setDanger] = useState(false)
+    const [amount, setAmount] = useState(0) 
+    const [breach, setBreach] = useState(false)
+    const tries = useRef(0);
+    
+    const [enteredPin, setEnteredPin] = useState(0);
+
+    const [account, dispatch]= useReducer(reducer,{amount:0, pin: 1234, access: false})
+    function reducer(account, action){
+      if(action.type == "GrantAccess"){return{...account, access: true}}
+     if(action.type == "Deposit"){return{...account, amount: account.amount+ amount}}
+      else if(action.type == "Withdraw"){return{...account, amount: account.amount- amount}}
+    }
+    function verifyPin(pin){
+      
+      if (pin == account.pin) {tries.current =0; dispatch({type:"GrantAccess"})}
+      else if(pin != account.pin && tries.current <3){tries.current++}
+      else if(pin != account.pin && tries.current >= 3){setBreach(true)}
+
+    }
+   
+    useEffect(() => {
+      if (breach){setDanger(true)}; //If condiotion is iportant because useEffect runs on initial render
+    }, [breach]);
+
+
+    if(danger == true){return(<div>Access denied</div>)}
+
+    else if(account.access == false && danger == false){
+      return(<div>
+        <input type="password" placeholder='Enter your pin' onChange={(e)=> setEnteredPin(Number(e.target.value))}/>
+        <button onClick={()=>verifyPin(enteredPin)} >Verify</button>
+      </div>)
+    }
+    else if (account.access == true){
+      return     (<div>
+      <div>{account.amount}</div>
+
+      <input type="text" value={amount} onChange={(e)=>setAmount(Number(e.target.value))}/>
+      <button onClick={()=>dispatch({type:"Deposit", payload: {amount}})}>Deposit</button>
+      <button onClick={ ()=>dispatch({type:"Withdraw", payload: {amount}})}>Withdraw</button>
+      </div>);}
+
+
+    
+    // else return (
+  
+      
+    // )
+  
       
   }
   //check for github
 
 
-  return (
-    <div>
-      <div>{redstate.count}</div>
-      <div>
-        <input type="text" value={redstate.incrementvalue} placeholder='Set increment' onChange={(e)=> dispatch({type:"setIncrement" , payload: Number(e.target.value)})} />
-        <button onClick={()=> dispatch({type: "increment", payload: 1})}>Increment</button> {/*calling the dispatch method requires an action object be passed*/ }
-        <button onClick={() => dispatch({type: "decrement", payload: 1})}>Decrement</button>
-      </div>  
-    </div>
-    
-  )
-}
+  
 
 export default App
