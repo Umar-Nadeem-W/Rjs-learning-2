c9294ef3a7d4c386273b51acef9922b4348dc360
Umar-Nadeem-W <umarnadeem.w@gmail.com>
2024-10-13 16:47:35 +0500

    Added more complex object use with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { useReducer, useState } from 'react'
 
 
 function App() {
-  const [redstate, dispatch] = useReducer(reducer,0) //first argument is a function and second argument is default state value in te useReducer hook
+  const [redstate, dispatch] = useReducer(reducer,{count: 0, incrementvalue:  1}) //first argument is a function and second argument is default state value in te useReducer hook
   function reducer(redstate, action){ // in the reducer function, first argument is the sate, second the action object
-      if(action.type=="increment"){return redstate + action.payload }
-      else if(action.type == "decrement"){return redstate- action.payload}
+      if(action.type=="increment"){return {...redstate, count: redstate.count + redstate.incrementvalue } }
+      else if(action.type == "decrement"){return {...redstate, count: redstate.count - redstate.incrementvalue }}
+      else if(action.type == "setIncrement"){return {...redstate, incrementvalue: action.payload }}
       
   }
   //check for github
-  
+
 
   return (
     <div>
-      <div>{redstate}</div>
+      <div>{redstate.count}</div>
       <div>
+        <input type="text" value={redstate.incrementvalue} placeholder='Set increment' onChange={(e)=> dispatch({type:"setIncrement" , payload: Number(e.target.value)})} />
         <button onClick={()=> dispatch({type: "increment", payload: 1})}>Increment</button> {/*calling the dispatch method requires an action object be passed*/ }
         <button onClick={() => dispatch({type: "decrement", payload: 1})}>Decrement</button>
       </div>  
